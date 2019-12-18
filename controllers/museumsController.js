d76b75540c0b6635311fb2d427603d3c85be5a6d
alberto.aguilar <faraond1@gmail.com>
2019-12-18 10:21:33 -0600

    djdj

--- controllers/museumsController.js
+++ controllers/museumsController.js
@@ -27,58 +27,66 @@ exports.index = function (req, res) {
 
 
 
-// Handle view museums info
-exports.view = function (req, res) {
-    Museums.findById(req.params.museums_id, function (err, museums) {
-        console.log("req.params.museums_id, IS: " + req.params.museums_id);
-        if (err)
-            res.send(err);
-           
-            res.json({
-            message: 'museums details loading..',
-           
-            data: museums
+
+exports.view= (req, res) => {
+    console.log("view"); 
+  // Validate request
+  if(! req.params.museums_id) {
+    return res.status(400).send({
+        message: "museum  museums_id can not be empty"
+    });
+}
+
+    Museums.findById (req.params.museums_id)
+    .then(museum => {
+        if(!museum) {
+            return res.status(404).send({
+                message: "Museum not found with id " +  req.params.museums_id,
+                status:'400',
+                data: err
+            });            
+        }
+        return res.status(200).send({
+            status: "success",
+            message: "Museum found",
+            data: museum
+        });
+   
+    }).catch(err => {
+        if(err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "Museum  not found with id " + req.params.museums_id,
+                status:'404',
+                data: err
+            });                
+        }
+        return res.status(500).send({
+            message: "Error retrieving Museum with id " + req.params.museums_id,
+            status:'500',
+            data: err
         });
     });
 };
 
 
 
-// Handle update user info
-exports.findOneAndUpdate = function (req, res) {
-    Museums.findOneAndUpdate(req.params.museums_id,
-        { $push: { Pieces : req.body.Pieces} } 
-        
-        ,function (err, museums) {
-   
-  
-    
-                console.log("req.params.museums_id IS: " +   req.params.museums_id); 
-                console.log("Pieces IS: " +   req.body.Pieces); 
-
-        if (err)
-            res.send(err);
-     
-   
-         
-           
-// save the user and check for errors
-            museums.save(function (err) {
-            if (err)
-                res.json(err);
-            res.json({
-                message: 'museums Info updated',
-                data: museums
-            });
-        });
+// Create and Save a new museum 
+exports.new= (req, res) => {
+    console.log("new  " ); 
+  // Validate request
+  if(!req.body) {
+    return res.status(400).send({
+        message: "Museum body can not be empty"
     });
-};
+}
 
+  //Validate request
+ /* if(!req.body.NameUser) {
+    return res.status(400).send({
+        message: "User NameUser can not be empty"
+    });
+    }*/
 
-
-
-// Handle create museums actions
-exports.new = function (req, res) {
     var museums = new Museums();
     museums.NameMuseum = req.body.NameMuseum ? req.body.NameMuseum : museums.NameMuseum;
     museums.DescriptMuseum = req.body.DescriptMuseum;
@@ -87,18 +95,22 @@ exports.new = function (req, res) {
 
 
 
-
-
-    console.log("REQ.BODY.lastname IS: " + req.body.NameMuseum);
-    console.log("REQ.BODY.lastname IS: " + req.body.Pieces);
-  
-// save the museums and check for errors
-museums.save(function (err) {
-         if (err)
-             res.json(err);
-res.json({
-            message: 'New museums created!',
+    // Save Museum in the database
+    museums.save()
+    .then(museums => {
+        return res.status(200).send({
+            message: 'New Musueum created!',
+            status:"success",
             data: museums
+        });
+    }).catch(err => {
+
+       
+        res.status(500).send({
+            message: err.message || "Some error occurred while creating the museum.",
+            status:'500',
+            data: err
+           
         });
     });
 };
@@ -106,20 +118,25 @@ res.json({
 
 
 
-// Handle update user info
-exports.update = function (req, res) {
-    Museums.findById(req.params.museums_id, function (err, museums) {
-        console.log("req.params.museums_id IS: " +   req.params.museums_id); 
+
+
+
+
+// Handle update museum info
+exports.findOneAndUpdate = function (req, res) {
+    Museums.findOneAndUpdate(req.params.museums_id,
+        { $push: { Pieces : req.body.Pieces} } 
+        
+        ,function (err, museums) { 
+    
+                console.log("req.params.museums_id IS: " +   req.params.museums_id); 
+                console.log("Pieces IS: " +   req.body.Pieces); 
 
         if (err)
             res.send(err);
-            museums.NameMuseum = req.body.NameMuseum ? req.body.NameMuseum : museums.NameMuseum;
-            museums.DescriptMuseum = req.body.DescriptMuseum;
-            museums.TokenMuseum = req.body.TokenMuseum;
-            museums.Pieces = req.body.Pieces;  
-         
-           
-// save the user and check for errors
+     
+              
+// save the museum  and check for errors
             museums.save(function (err) {
             if (err)
                 res.json(err);
@@ -133,25 +150,108 @@ exports.update = function (req, res) {
 
 
 
+// Update a museum identified by the MuseumId in the request
+exports.update = (req, res) => {
+
+    console.log("update  " +   req.params.museums_id); 
+    // Validate Request
+    if(!req.params.museums_id) {
+        return res.status(400).send({
+            message: "Museum id can not be empty"
+        });
+    }
 
 
+      // Validate Request
+      if(!req.body) {
+        console.log("update  " +  req.body); 
+        return res.status(400).send({
+            message: "Museum body can not be empty"
+        });
+    }
 
-// Handle delete user
-exports.delete = function (req, res) {
-    Museums.remove({
-   
-    }, 
-    
-
-    function (err, user) {
-        console.log("req.params.museums_id IS: " + req.params.museums_id); 
-        if (err)
-            res.send(err);
-res.json({
-            status: "success",
-            message: 'museums deleted'
+    // Find Museum and update it with the request body
+    Museums.findByIdAndUpdate(req.params.museums_id, {
+        NameMuseum : req.body.NameMuseum ? req.body.NameMuseum : museums.NameMuseum,
+        DescriptMuseum : req.body.DescriptMuseum,
+        TokenMuseum : req.body.TokenMuseum,
+        Pieces : req.body.Pieces,  
+    }, {new: true})
+    .then(museums => {
+        if(!museums) {
+            return res.status(404).send({
+                message: "Museum not found with id " + req.params.museums_id,
+                    status:'404',
+                    data: err
+            });
+        }
+        return res.status(200).send({
+            message: 'Museum Info updated',
+            status:"success",
+            data: museums
+        });
+      
+           
+    }).catch(err => {
+        if(err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "Museum not found with id " + req.params.museums_id,
+                status:'404',
+                data: err
+            });                
+        }
+        return res.status(500).send({
+            message: "Error updating museum with id " + req.params.museums_id,
+            status:'500',
+            data: err
         });
     });
 };
+
+
+
+
+// Delete a Museum with the specified MuseumId in the request
+exports.delete = (req, res) => {
+    console.log("delete  " +  req.params.museums_id); 
+    if(!req.params.museums_id) {
+        return res.status(400).send({
+            message: "Museum content can not be empty"
+        });
+    }
+
+    Museums.findByIdAndRemove(req.params.museums_id)
+    .then(museums => {
+        if(!museums) {
+            return res.status(404).send({
+                message: "Museum  not found with id " + req.params.museums_id,
+                status:'404',
+                data: err
+            });
+        }
+        res.send({  
+            message: "Musueum deleted successfully!",
+            status:"success",
+            data: museums
+        });
+    }).catch(err => {
+        if(err.kind === 'ObjectId' || err.name === 'NotFound') {
+            return res.status(404).send({
+                message: "Museum not found with id " + req.params.museums_id,
+                status:'404',
+                data: err
+            });                
+        }
+        return res.status(500).send({
+            message: "Could not delete Museum with id " + req.params.museums_id,
+            status:'500',
+            data: err
+        });
+    });
+};
+
+
+
+
 
 
--- controllers/piecesController.js
+++ controllers/piecesController.js
@@ -23,96 +23,202 @@ exports.index = function (req, res) {
 };
 
 
-
-
-
 // Handle view pieces info
-exports.view = function (req, res) {
-    Pieces.findById(req.params.pieces_id, function (err, pieces) {
-        console.log("req.params.pieces_id, IS: " + req.params.pieces_id);
-        if (err)
-            res.send(err);
-           
-            res.json({
-            message: 'pieces details loading..',
-           
+exports.view= (req, res) => {
+    console.log("view"); 
+  // Validate request
+  if(!req.params.pieces_id) {
+    return res.status(400).send({
+        message: "Piece  pieces_id can not be empty"
+    });
+}
+
+    Pieces.findById(req.params.pieces_id)
+    .then(pieces => {
+        if(!pieces) {
+            return res.status(404).send({
+                message: "Pieces not found with id " + req.params.pieces_id,
+                status:'400',
+                data: err
+            });            
+        }
+        return res.status(200).send({
+            status: "success",
+            message: "Pieces found",
             data: pieces
+        });
+   
+    }).catch(err => {
+        if(err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "Pieces not found with id " + req.params.pieces_id,
+                status:'404',
+                data: err
+            });                
+        }
+        return res.status(500).send({
+            message: "Error retrieving Pieces with id " + req.params.pieces_id,
+            status:'500',
+            data: err
         });
     });
 };
 
 
-// Handle create user actions
-exports.new = function (req, res) {
+
+
+// Create and Save a new pieces
+exports.new= (req, res) => {
+    console.log("new  " ); 
+  // Validate request
+  if(!req.body) {
+    return res.status(400).send({
+        message: "Piece body can not be empty"
+    });
+}
+
+  //Validate request
+ /* if(!req.body.NameUser) {
+    return res.status(400).send({
+        message: "User NameUser can not be empty"
+    });
+    }*/
+
+
     var pieces = new Pieces();
     pieces.NamePieces = req.body.NamePieces ? req.body.NamePieces : museums.NamePieces;
     pieces.DescripPieces = req.body.DescripPieces;
     pieces.ImgPieces = req.body.ImgPieces;
-    pieces.TokenPiece = req.body.TokenPiece;
+    pieces.TokenPiece = req.body.TokenPiece
 
 
 
-
-
-
-
-    console.log("REQ.BODY.lastname IS: " + req.body.NamePieces);
-    console.log("REQ.BODY.lastname IS: " + req.body.DescripPieces);
-  
-// save the user and check for errors
-pieces.save(function (err) {
-         if (err)
-             res.json(err);
-res.json({
-            message: 'New pieces created!',
+    // Save Pieces in the database
+    pieces.save()
+    .then(pieces => {
+        return res.status(200).send({
+            message: 'New piece created!',
+            status:"success",
             data: pieces
         });
-    });
-};
+    }).catch(err => {
 
-
-
-
-// Handle update user info
-exports.update = function (req, res) {
-    Pieces.findById(req.params.pieces_id, function (err, pieces) {
-        console.log("req.params.pieces_id IS: " +   req.params.pieces_id); 
-
-        if (err)
-            res.send(err);
-            pieces.NamePieces = req.body.NamePieces ? req.body.NamePieces : museums.NamePieces;
-            pieces.DescripPieces = req.body.DescripPieces;
-            pieces.ImgPieces = req.body.ImgPieces;
-            pieces.TokenPiece = req.body.TokenPiece;
-         
+       
+        res.status(500).send({
+            message: err.message || "Some error occurred while creating the piece.",
+            status:'500',
+            data: err
            
-// save the user and check for errors
-pieces.save(function (err) {
-            if (err)
-                res.json(err);
-            res.json({
-                message: 'pieces Info updated',
-                data: pieces
-            });
         });
     });
 };
-// Handle delete user
-exports.delete = function (req, res) {
-    Pieces.remove({
-   
-    }, 
-    
 
-    function (err, user) {
-        console.log("req.params.pieces_id IS: " + req.params.museums_id); 
-        if (err)
-            res.send(err);
-res.json({
-            status: "success",
-            message: 'pieces deleted'
+
+
+
+
+// Update a piece identified by the PieceId in the request
+exports.update = (req, res) => {
+
+    console.log("req.params.pieces_id IS: " +   req.params.pieces_id);  
+    // Validate Request
+    if(! req.params.pieces_id) {
+        return res.status(400).send({
+            message: "Piece id can not be empty"
+        });
+    }
+
+
+      // Validate Request
+      if(!req.body) {
+        console.log("update  " +  req.body); 
+        return res.status(400).send({
+            message: "Piece body can not be empty"
+        });
+    }
+
+    // Find Pieces and update it with the request body
+    Pieces.findByIdAndUpdate( req.params.pieces_id, {
+        NamePieces : req.body.NamePieces ? req.body.NamePieces : museums.NamePieces,
+        DescripPieces : req.body.DescripPieces,
+        ImgPieces : req.body.ImgPieces,
+        TokenPiece : req.body.TokenPiece
+
+
+    }, {new: true})
+    .then(pieces => {
+        if(!pieces) {
+            return res.status(404).send({
+                message: "Pieces not found with id " +  req.params.pieces_id,
+                    status:'404',
+                    data: err
+            });
+        }
+        return res.status(200).send({
+            message: 'pieces Info updated',
+            status:"success",
+            data: pieces
+        });
+      
+           
+    }).catch(err => {
+        if(err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "Pieces not found with id " +  req.params.pieces_id,
+                status:'404',
+                data: err
+            });                
+        }
+        return res.status(500).send({
+            message: "Error updating piece with id " +  req.params.pieces_id,
+            status:'500',
+            data: err
         });
     });
 };
+
+
+
+
+// Delete a Oieces with the specified PiecesId in the request
+exports.delete = (req, res) => {
+    console.log("delete  " +  req.params.pieces_id ); 
+    if(!req.params.pieces_id) {
+        return res.status(400).send({
+            message: "piece_id  can not be empty"
+        });
+    }
+
+    Pieces.findByIdAndRemove(req.params.pieces_id)
+    .then(pieces => {
+        if(!users) {
+            return res.status(404).send({
+                message: "Piece not found with id " + req.params.pieces_id,
+                status:'404',
+                data: err
+            });
+        }
+        res.send({  
+            message: "Piece deleted successfully!",
+            status:"success",
+            data: pieces
+        });
+    }).catch(err => {
+        if(err.kind === 'ObjectId' || err.name === 'NotFound') {
+            return res.status(404).send({
+                message: "Pieces not found with id " + req.params.pieces_id,
+                status:'404',
+                data: err
+            });                
+        }
+        return res.status(500).send({
+            message: "Could not delete Piece with id " + req.params.pieces_id,
+            status:'500',
+            data: err
+        });
+    });
+};
+
+
 
 
